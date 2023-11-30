f5761c49d54722179bd23c9ebe2f7eb97e6412c8
TheXWitcher <87780850+TheXWitcher@users.noreply.github.com>
2023-11-30 16:11:11 +0400

    Create IRequestRepository.cs

---
+++ src/Server/Students.APIServer/Repository/IRequestRepository.cs
@@ -0,0 +1,13 @@
+using Students.Models;
+
+
+namespace Students.APIServer.Repository
+{
+
+    public interface IRequestRepository : IGenericRepository<Request>
+    {
+        Task<IEnumerable<Request>> FindRequesListByStudentGuidAsync(Guid id);
+        Task<Request> FindRequestByPhoneFromRequestAsync(Request request);
+        Task<Request> FindRequestByEmailFromRequestAsync(Request request);
+    }
+}
