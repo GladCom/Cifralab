ee9d0c887e7149e684f64fcb3f0f94f58564ac4d
TheXWitcher <87780850+TheXWitcher@users.noreply.github.com>
2023-11-30 21:33:40 +0400

    Update RequestController.cs

--- src/Server/Students.APIServer/Controllers/RequestController.cs
+++ src/Server/Students.APIServer/Controllers/RequestController.cs
@@ -1,7 +1,6 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using Students.APIServer.Repository;
-using Students.APIServer.Services;
 using Students.Models;
 using System;
 using System.Diagnostics;
@@ -11,132 +10,21 @@ namespace Students.APIServer.Controllers;
 [ApiController]
 [Route("[controller]")]
 [ApiVersion("1.0")]
-public class RequestController : Controller
+public class RequestController : GenericAPiController<Request>
 {
-    //private readonly IRequestRepository _requestRepository;
+    
     private readonly ILogger<Request> _logger;
-    private readonly IRequestService _requestService;
-    public RequestController(IRequestService service, ILogger<Request> logger) 
+    private readonly IRequestRepository _requestRepository;
+    public RequestController(IGenericRepository<Request> repository, ILogger<Request> logger, IRequestRepository requestRepository) : base(repository, logger)
     {
-        //_requestRepository = requestRepository;
+        _requestRepository = requestRepository;
         _logger = logger;
-        _requestService = service;
     }
-    [HttpGet("GetByGUID")]
-    public async Task<IActionResult> FindById(Guid id)
-    {
-
-        try
-        {
-            var form = await _requestService.FindById(id);
-            if (form == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound,
-                    new DefaultResponse
-                    {
-                        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                    });
-            }
-
-            return StatusCode(StatusCodes.Status200OK, form);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while getting Entity by Id");
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new DefaultResponse
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                });
-        }
-    }
-    [HttpPost("GetRequestByPhoneAsync")]
-    public async Task<IActionResult> FindRequestByPhoneFromRequestAsync(Request item)
+    public override async Task<IActionResult> Get(Guid id)
     {
         try
         {
-            var form = await _requestService.FindRequestByPhoneFromRequestAsync(item);
-            if (form == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound,
-                    new DefaultResponse
-                    {
-                        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                    });
-            }
-
-            return StatusCode(StatusCodes.Status200OK, form);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while getting Entity by Id");
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new DefaultResponse
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                });
-        }
-    }
-    [HttpPost("GetRequestByEmailAsync")]
-    public async Task<IActionResult> FindRequestByEmailFromRequestAsync(Request item)
-    {
-        try
-        {
-            var form = await _requestService.FindRequestByEmailFromRequestAsync(item);
-            if (form == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound,
-                    new DefaultResponse
-                    {
-                        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                    });
-            }
-
-            return StatusCode(StatusCodes.Status200OK, form);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while getting Entity by Id");
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new DefaultResponse
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                });
-        }
-    }
-    [HttpGet("GetAll")]
-    public async Task<IActionResult> Get()
-    {
-        try
-        {
-            var form = await _requestService.Get();
-            if (form == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound,
-                    new DefaultResponse
-                    {
-                        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                    });
-            }
-
-            return StatusCode(StatusCodes.Status200OK, form);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while getting Entity by Id");
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new DefaultResponse
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                });
-        }
-    }
-    [HttpGet("GetAll2")]
-    public async Task<IActionResult> Get(Func<Request, bool> predicate)
-    {
-        try
-        {
-            var form = await _requestService.Get(predicate);
+            var form = await _requestRepository.FindById(id);
             if (form == null)
             {
                 return StatusCode(StatusCodes.Status404NotFound,
@@ -159,85 +47,11 @@ public class RequestController : Controller
         }
     }
 
-    [HttpGet("FindRequesListByStudentGuidAsync")]
-    public async Task<IActionResult> FindRequesListByStudentGuidAsync(Guid id)
+    public override async Task<IActionResult> Post(Request request)
     {
         try
         {
-            var form = await _requestService.FindRequesListByStudentGuidAsync(id);
-            if (form == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound,
-                    new DefaultResponse
-                    {
-                        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                    });
-            }
-
-            return StatusCode(StatusCodes.Status200OK, form);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while getting Entity by Id");
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new DefaultResponse
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                });
-        }
-    }
-    [HttpPost("Create")]
-    public async Task<IActionResult> Create(Request item)
-    {
-        try
-        {
-            var form = await _requestService.Create(item);
-            if (form == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound,
-                    new DefaultResponse
-                    {
-                        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                    });
-            }
-
-            return StatusCode(StatusCodes.Status200OK, form);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while getting Entity by Id");
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new DefaultResponse
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                });
-        }
-    }
-    [HttpDelete("Remove")]
-    public async Task<IActionResult> Remove(Request item)
-    {
-        try
-        {
-            await _requestService.Remove(item);
-            
-            return StatusCode(StatusCodes.Status200OK);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error while getting Entity by Id");
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                new DefaultResponse
-                {
-                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-                });
-        }
-    }
-    [HttpPut("Update")]
-    public async Task<IActionResult> Update(Request item)
-    {
-        try
-        {
-            var form = await _requestService.Update(item);
+            var form = await _requestRepository.Create(request);
             if (form == null)
             {
                 return StatusCode(StatusCodes.Status404NotFound,
