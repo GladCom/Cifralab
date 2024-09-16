73968f2125efd0c354bc15281b907b6d8f88cd61
Алексей Трусов <aleksey.trusov@codesphere.ru>
2024-09-16 19:10:57 +0400

    try fix #19

--- build/image-tag.sh
+++ build/image-tag.sh
@@ -4,11 +4,11 @@
 # Берем имя ветки из переменной CI_COMMIT_TITLE, там будет что-то подобное:
 # "Merge branch 'my_cool_branch' into 'main'"
 echo ${1}
-IMAGE_TAG_NAME=$(echo "${1}" | cut -d ' ' -f 3)
-echo ${IMAGE_TAG_NAME}
-IMAGE_TAG_NAME="${IMAGE_TAG_NAME:1:-1}"
-echo ${IMAGE_TAG_NAME}
+IMAGE_TAG=$(echo "${1}" | cut -d ' ' -f 3)
+echo ${IMAGE_TAG}
+IMAGE_TAG="${IMAGE_TAG:1:-1}"
+echo ${IMAGE_TAG}
 
 # Убераем из имени некоторые сиволы допустимы для веток git но не для тегов docker
-IMAGE_TAG_NAME=$(echo "${IMAGE_TAG_NAME}" | tr -d "{}[]:;,")
-echo $IMAGE_TAG_NAME
+IMAGE_TAG=$(echo "${IMAGE_TAG}" | tr -d "{}[]:;,")
+echo $IMAGE_TAG
