86acaf8a647c2c8a27c3ac96c056260024788e72
Alexandr Artsybashev <a_arz@mail.ru>
2024-09-22 22:30:47 +0400

    Логика локальной пагинации вынесена в отдельный модуль

--- src/Client/src/services/paginator.js
+++ src/Client/src/services/paginator.js
@@ -4,7 +4,7 @@ export const getDataForPage = (data, currentPage, pageSize) => {
     try {
         const chunks = _.chunk(data, pageSize);
         const dataChunks = chunks.map((items, index) => ({ items, pageNumber: index }));
-
+        
         return dataChunks[currentPage]?.items;
       } catch (e) {
         console.error(e);
