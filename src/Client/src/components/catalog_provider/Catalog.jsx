88b7018224c7255b4d304f4e5671848c59219987
Alexandr Artsybashev <a_arz@mail.ru>
2024-09-25 00:54:31 +0400

    Указан правильный адрес сервера

--- src/Client/src/components/catalog_provider/Catalog.jsx
+++ src/Client/src/components/catalog_provider/Catalog.jsx
@@ -3,14 +3,45 @@ import FilterPanel from './FilterPanel.jsx';
 import DataPanel from './DataPanel.jsx';
 import Spinner from '../shared/Spinner.jsx';
 import Empty from '../shared/Empty.jsx';
+import Error from '../shared/Error.jsx';
+import {  Pagination  }  from 'antd';
 
 
 
 const Catalog = ({ config }) => {
     const { columns, catalogData } = config;
-    const { getAsync } = catalogData;
-    const { data, error, isLoading, refetch } = getAsync();
-    const [filteredData, setFilteredData] = useState([]);
+    const { getAllPagedAsync } = catalogData;
+    const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+    const [queryString, setQueryString] = useState('');
+    const [query, setQuery] = useState({});
+
+    const { 
+        data,
+        error,
+        isLoading,
+        isFetching,
+        refetch
+    } = getAllPagedAsync({ pageNumber, pageSize, filterDataReq: queryString });
+
+    const onShowSizeChange = useCallback((current, pageSize) => {
+        setPageNumber(current);
+        setPageSize(pageSize);
+    });
+
+    const onCurrentPageChange = useCallback((page, pageSize) => {
+        setPageNumber(page);
+        setPageSize(pageSize);
+    });
+
+    useEffect(() => {
+        let queryString = '';
+        for (const [key, value] of Object.entries(query)) {
+            queryString += `&${key}=${value}`
+        }
+        setPageNumber(1);
+        setQueryString(queryString);
+    }, [query]);
 
     if (isLoading) {
         return (
@@ -21,14 +52,31 @@ const Catalog = ({ config }) => {
         );
     }
 
+    if (error) {
+        return <Error e={error} />;
+    }
+console.log(data)
     return ( 
         <div>
             <FilterPanel
                 columns={columns}
+                query={query}
                 notFilteredData={data.data}
-                filterData={setFilteredData}
+                setQuery={setQuery}
             />
-            <DataPanel columns={columns} data={filteredData} />
+            <DataPanel columns={columns} data={data?.data} />
+            <br />
+            <Pagination
+                className="mb-3"
+                showSizeChanger
+                hideOnSinglePage
+                onChange={onCurrentPageChange}
+                onShowSizeChange={onShowSizeChange}
+                current={pageNumber}
+                defaultCurrent={1}
+                total={data?.totalCount}
+            />
+            {isFetching && <Spinner />}
         </div>
     );
 };
