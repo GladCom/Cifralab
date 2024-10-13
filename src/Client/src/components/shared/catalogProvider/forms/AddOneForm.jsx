5c85555c652bb9e8cd9a56209c0f9c42035e1f3e
ivanmirable <ivankrivonogov1823@mail.ru>
2024-10-13 21:06:50 +0400

    feat: функционал валидации для компонентов: String,Email,Snils

--- src/Client/src/components/shared/catalogProvider/forms/AddOneForm.jsx
+++ src/Client/src/components/shared/catalogProvider/forms/AddOneForm.jsx
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Stack from 'react-bootstrap/Stack';
-import { notification } from 'antd';
+
 
 const RequiredSymbol = () => {
   return <span className="text-danger">* </span>;
