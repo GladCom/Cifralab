7802990d90ab716d28cb5795f91d592877683076
Alex Arts <a_arz@mail.ru>
2024-10-15 11:05:54 +0000

    Добавлены недостающие поля в программу обучения

--- src/Client/src/storage/catalogConfigs/educationPrograms.js
+++ src/Client/src/storage/catalogConfigs/educationPrograms.js
@@ -26,6 +26,9 @@ export default {
         feaProgramId: { name: 'ВЭД программы', type: FEAProgramSelect, show: true, required: true },
         financingTypeId: { name: 'Источник финансирования', type: FinancingTypeSelect, show: true, required: true },
         isCollegeProgram: { name: 'Обязательно наличие ВО', type: YesNoSelect, show: true, required: true },
+        isNetworkProgram: { name: 'Сетевая форма', type: YesNoSelect, show: true, required: true },
+        isDOTProgram: { name: 'Применение ДОТ', type: YesNoSelect, show: true, required: true },
+        isFullDOTProgram: { name: 'Применение ДОТ полностью', type: YesNoSelect, show: true, required: true },
         isArchive: { name: 'В архиве', type: YesNoSelect, show: true, required: true },
     },
     fields: [
