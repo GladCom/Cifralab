e6b1a74487c73fc4c81082c8236c1bdc2ac19101
Alexandr Artsybashev <a_arz@mail.ru>
2024-11-04 20:36:46 +0400

    Рефакторинг роутинга с целью применения DataRouter
    В новой версии реакт-роутера чтобы пользоваться некоторыми хуками, нужно по другому инициализировать дата-провайдер. В данном случае дата-провайдер нужен для хука useBlocker

--- src/Client/src/index.jsx
+++ src/Client/src/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import store from './storage/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+const router = createBrowserRouter(createRoutesFromElements(<Route path='*' element={<App />} />));
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
