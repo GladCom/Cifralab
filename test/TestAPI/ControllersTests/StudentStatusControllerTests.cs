1d48ffe8fcf61e946c486634ec5cd5d973d69801
Сунгатуллин Ринат <rinat.sun.9952@gmail.com>
2024-11-13 17:42:37 +0000

    #166. Тесты на StudentStatusController

---
+++ test/TestAPI/ControllersTests/StudentStatusControllerTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TestAPI.ControllersTests;
+
+[TestFixture]
+public class StudentStatusControllerTests
+{
+  [SetUp]
+  public void SetUp()
+  {
+  }
+
+  [TearDown]
+  public void TearDown()
+  {
+  }
+}
