87e0345a782a7efef5471f459e22aefff03a4bb5
Serj1525 <serj1525@yandex.ru>
2025-10-29 19:41:41 +0400

    Изменен порядок и названия полей в соответствии с макетом

--- src/Client/src/storage/models/education-programs.ts
+++ src/Client/src/storage/models/education-programs.ts
@@ -9,25 +9,25 @@ import KindEducationProgramSelect from '../../components/shared/business/selects
 import FinancingTypeSelect from '../../components/shared/business/selects/financing-type-select';
 
 const model = {
-  name: { name: 'Программа обучения', type: String, show: true, required: true },
-  cost: { name: 'Стоимость', type: Cost, show: true, required: true },
-  hoursCount: { name: 'Кол-во часов', type: HoursCount, show: true, required: true },
-  educationFormId: { name: 'Форма образования', type: EducationFormSelect, show: true, required: true },
+  name: { name: 'Наименование', type: String, show: true, required: true },
+  hoursCount: { name: 'Количество часов', type: HoursCount, show: true, required: true },
+  educationFormId: { name: 'Форма обучения', type: EducationFormSelect, show: true, required: true },
   kindEducationProgramId: { name: 'Вид программы', type: KindEducationProgramSelect, show: true, required: true },
+  cost: { name: 'Стоимость обучения', type: Cost, show: true, required: true },
+  financingTypeId: { name: 'Источник финансирования', type: FinancingTypeSelect, show: true, required: true },
+  isModularProgram: { name: 'Модульная программа', type: YesNo, show: true, required: true },
+  feaProgramId: { name: 'ВЭД программы', type: FEAProgramSelect, show: true, required: true },
+  isNetworkProgram: { name: 'Сетевая форма', type: YesNo, show: true, required: true },
+  isDOTProgram: { name: 'Применение ДОТ', type: YesNo, show: true, required: true },
+  isFullDOTProgram: { name: 'Применение ДОТ полностью', type: YesNo, show: true, required: true },
+  isCollegeProgram: { name: 'Обязательно наличие ВО', type: YesNo, show: true, required: true },
+  qualificationName: { name: 'Наименование квалификации, профессии, специальности', type: String, show: true, required: true },
   kindDocumentRiseQualificationId: {
     name: 'Вид документа',
     type: KindDocumentRiseQualificationSelect,
     show: true,
     required: true,
   },
-  isModularProgram: { name: 'Модульная программа', type: YesNo, show: true, required: true },
-  feaProgramId: { name: 'ВЭД программы', type: FEAProgramSelect, show: true, required: true },
-  financingTypeId: { name: 'Источник финансирования', type: FinancingTypeSelect, show: true, required: true },
-  isCollegeProgram: { name: 'Обязательно наличие ВО', type: YesNo, show: true, required: true },
-  isNetworkProgram: { name: 'Сетевая форма', type: YesNo, show: true, required: true },
-  isDOTProgram: { name: 'Применение ДОТ', type: YesNo, show: true, required: true },
-  isFullDOTProgram: { name: 'Применение ДОТ полностью', type: YesNo, show: true, required: true },
-  qualificationName: { name: 'Наименование квалификации', type: String, show: true, required: true },
   isArchive: { name: 'В архиве', type: YesNo, show: true, required: true },
 };
 
