f150dbdec6742734a4a8f875fb8fba19e878750a
Zverev_ID <helix999@mail.ru>
2025-11-27 22:53:29 +0400

    Реализовал фильтр по фамилии

--- src/Students.Models.Filters/Filters/StudentFilter.cs
+++ src/Students.Models.Filters/Filters/StudentFilter.cs
@@ -1,8 +1,11 @@
 ﻿namespace Students.Models.Filters.Filters;
 public class StudentFilter : Filter<Student>
 {
+  public string? Family { get; set; }
+  
   public override Predicate<Student> GetFilterPredicate()
   {
-    throw new NotImplementedException();
+    return
+      x => (string.IsNullOrEmpty((this.Family)) || this.Family.ToLower() == x.Family.ToLower());
   }
 }
