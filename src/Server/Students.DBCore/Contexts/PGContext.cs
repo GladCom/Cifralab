d508044f32cee68924552a1b885f22b426e70860
vanich12 <ivankrivonogov1823@mail.ru>
2026-01-11 21:38:39 +0400

    исправил получение данных о студенте при формировании отчёта ФРДО

--- src/Server/Students.DBCore/Contexts/PGContext.cs
+++ src/Server/Students.DBCore/Contexts/PGContext.cs
@@ -7,11 +7,11 @@ public sealed class PgContext : WorkContext
 {
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    optionsBuilder.UseNpgsql(
-      $"Host={Environment.GetEnvironmentVariable("DBServer")};Port={Environment.GetEnvironmentVariable("DBPort")};Database={Environment.GetEnvironmentVariable("DBName")};Username={Environment.GetEnvironmentVariable("DBLogin")};Password={Environment.GetEnvironmentVariable("DBPassword")};",
-      o => o.CommandTimeout(60));
+      optionsBuilder.UseNpgsql(
+          $"Host=localhost;Port=5432;Database=Cifralabs_local;Username=postgres;Password=123;",
+          o => o.CommandTimeout(60));
 #if DEBUG
-    optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
 #endif
   }
 
--- src/Students.Report/Repositories/FRDOReportRepository.cs
+++ src/Students.Report/Repositories/FRDOReportRepository.cs
@@ -13,86 +13,90 @@ namespace Students.Reports.Repositories;
 /// </summary>
 public class FRDOReportRepository : BaseReportRepository<FRDOModel>
 {
-  /// <summary>
-  ///   Формирование данных для отчета.
-  /// </summary>
-  /// <param name="filter">Фильтр</param>
-  /// <returns>Данные.</returns>
-  public async Task<List<FRDOModel>> Get(GroupFilter filter)
-  {
-    return await this.FetchData(filter.GetFilterPredicate());
-  }
-
-  /// <summary>
-  ///   Извлечение данных.
-  /// </summary>
-  /// <param name="condition">Условие</param>
-  /// <returns>Список данных отчета.</returns>
-  protected override async Task<List<FRDOModel>> FetchData(Predicate<Group> condition)
-  {
-        var frdoModels = new List<FRDOModel>();
-
-        // 1. Делаем запрос
-        var query = this.Context.Groups
-            .Include(group => group.Students)
-                .ThenInclude(student => student.TypeEducation)
-
-            .Include(group => group.Students)
-                .ThenInclude(student => student.GroupStudent)
-            .Include(group => group.GroupStudent)
-
-            .Include(group => group.EducationProgram)
-                .ThenInclude(ep => ep!.FEAProgram)
-            .Include(group => group.EducationProgram)
-                .ThenInclude(ep => ep!.EducationForm)
-            .Include(group => group.EducationProgram)
-                .ThenInclude(ep => ep!.FinancingType)
-            .Include(group => group.EducationProgram)
-                .ThenInclude(ep => ep!.KindDocumentRiseQualification)
-            .AsNoTracking()
-            .AsAsyncEnumerable();
-
-        int totalGroups = 0;
-
-        await foreach (var group in query)
-        {
-            totalGroups++;
-
-            bool result = condition(group);
-
-            if (result)
-                frdoModels.AddRange(group.Students.Select(student => InitializeObject(student, group)));
-        }
-
-        if (totalGroups == 0)
-            throw new Exception("В базе вообще нет групп! Проверь ConnectionString.");
-
-        return frdoModels;
+    /// <summary>
+    ///   Формирование данных для отчета.
+    /// </summary>
+    /// <param name="filter">Фильтр</param>
+    /// <returns>Данные.</returns>
+    public async Task<List<FRDOModel>> Get(GroupFilter filter)
+    {
+        return await this.FetchData(filter.GetFilterPredicate());
     }
 
-  public static Form1PKModel CalculateStatistics(List<Student> allStudents)
-  {
-      var stats = new Form1PKModel();
+    /// <summary>
+    ///   Извлечение данных.
+    /// </summary>
+    /// <param name="condition">Условие</param>
+    /// <returns>Список данных отчета.</returns>
+    protected override async Task<List<FRDOModel>> FetchData(Predicate<Group> condition)
+    {
+        try
+        {
+            var frdoModels = new List<FRDOModel>();
 
-      stats.TotalListeners = allStudents.Count;
+            // 1. Делаем запрос
+            var query = this.Context.Groups
+                .Include(group => group.Students)
+                .ThenInclude(student => student.TypeEducation)
+                .Include(group => group.GroupStudent)
+                .ThenInclude(student => student.Student)
+                .Include(group => group.EducationProgram)
+                .ThenInclude(ep => ep!.FEAProgram)
+                .Include(group => group.EducationProgram)
+                .ThenInclude(ep => ep!.EducationForm)
+                .Include(group => group.EducationProgram)
+                .ThenInclude(ep => ep!.FinancingType)
+                .Include(group => group.EducationProgram)
+                .ThenInclude(ep => ep!.KindDocumentRiseQualification)
+                .AsNoTracking()
+                .AsAsyncEnumerable();
 
-      stats.WomenCount = allStudents.Count(s => s.Sex == SexHuman.Woman);
+            int totalGroups = 0;
 
-      foreach (var student in allStudents)
-      {
-          var age =student.Age; 
+            await foreach (var group in query)
+            {
+                totalGroups++;
 
-          if (age < 25) stats.AgeUnder25++;
-          else if (age <= 29) stats.Age25_29++;
-          else if (age <= 34) stats.Age30_34++;
-          else if (age <= 39) stats.Age35_39++;
-          else if (age <= 44) stats.Age40_44++;
+                bool result = condition(group);
 
-      }
+                if (result)
+                    frdoModels.AddRange(group.GroupStudent.Select(gs => InitializeObject(gs.Student, gs.Group)));
+            }
+
+            if (totalGroups == 0)
+                throw new Exception("В базе вообще нет групп! Проверь ConnectionString.");
+
+            return frdoModels;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public static Form1PKModel CalculateStatistics(List<Student> allStudents)
+    {
+        var stats = new Form1PKModel();
+
+        stats.TotalListeners = allStudents.Count;
+
+        stats.WomenCount = allStudents.Count(s => s.Sex == SexHuman.Woman);
+
+        foreach (var student in allStudents)
+        {
+            var age = student.Age;
+
+            if (age < 25) stats.AgeUnder25++;
+            else if (age <= 29) stats.Age25_29++;
+            else if (age <= 34) stats.Age30_34++;
+            else if (age <= 39) stats.Age35_39++;
+            else if (age <= 44) stats.Age40_44++;
+        }
 
 
-      return stats;
-  }
+        return stats;
+    }
 
     /// <summary>
     ///   Инициализация свойств оъекта.
@@ -101,39 +105,41 @@ public class FRDOReportRepository : BaseReportRepository<FRDOModel>
     /// <param name="group">Сущность.</param>
     /// <returns>Сущность.</returns>
     private static FRDOModel InitializeObject(Student student, Group group)
-  {
-    var empty = string.Empty;
-    return new FRDOModel
     {
-      TypeDocument = group.EducationProgram!.KindDocumentRiseQualification!.Name,
-      SeriesDocuments = empty,
-      DocumentNumber = empty,
-      DateOfIssueDocument = empty,
-      RegistrationNumber = empty,
-      AdditionalProfessionalProgram = empty,
-      NameAdditionalProfessionalProgram = group.EducationProgram!.Name,
-      NameQualification = group.EducationProgram!.FEAProgram!?.Name ?? empty,
-      LevelEducationHE = student.TypeEducation!.Name,
-      SurnameIndicatedHE = student.Family,
-      SeriesHE = student.DocumentSeries,
-      NumberHE = student.DocumentNumber,
-      YearBeginningTraining = group.StartDate.ToString(),
-      YearGraduation = group.EndDate.ToString(),
-      DurationTraining = group.EducationProgram!.HoursCount.ToString(),
-      RecipientLastName = student.Family,
-      RecipientName = student.Name,
-      RecipientPatronymic = student.Patron,
-      RecipientDateBirth = student.BirthDate.ToString(),
-      RecipientGender = student.Sex == SexHuman.Woman ? "Жен." : "Муж.",
-      RecipientSNILS = student.SNILS,
-      FormEducation = group.EducationProgram.EducationForm!.Name,
-      SourceFundingForTraining = group.EducationProgram.FinancingType!.SourceName
-    };
-  }
+        var empty = string.Empty;
+        return new FRDOModel
+        {
+            TypeDocument = group.EducationProgram!.KindDocumentRiseQualification!.Name,
+            SeriesDocuments = empty,
+            DocumentNumber = empty,
+            DateOfIssueDocument = empty,
+            RegistrationNumber = empty,
+            AdditionalProfessionalProgram = empty,
+            NameAdditionalProfessionalProgram = group.EducationProgram!.Name,
+            NameQualification = group.EducationProgram!.FEAProgram!?.Name ?? empty,
+            LevelEducationHE = student.TypeEducation!.Name,
+            SurnameIndicatedHE = student.Family,
+            SeriesHE = student.DocumentSeries,
+            NumberHE = student.DocumentNumber,
+            YearBeginningTraining = group.StartDate.ToString(),
+            YearGraduation = group.EndDate.ToString(),
+            DurationTraining = group.EducationProgram!.HoursCount.ToString(),
+            RecipientLastName = student.Family,
+            RecipientName = student.Name,
+            RecipientPatronymic = student.Patron,
+            RecipientDateBirth = student.BirthDate.ToString(),
+            RecipientGender = student.Sex == SexHuman.Woman ? "Жен." : "Муж.",
+            RecipientSNILS = student.SNILS,
+            FormEducation = group.EducationProgram.EducationForm!.Name,
+            SourceFundingForTraining = group.EducationProgram.FinancingType!.SourceName
+        };
+    }
 
-  /// <summary>
-  /// Конструктор.
-  /// </summary>
-  /// <param name="context">Контекст.</param>
-  public FRDOReportRepository(StudentContext context) : base(context) { }
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="context">Контекст.</param>
+    public FRDOReportRepository(StudentContext context) : base(context)
+    {
+    }
 }
